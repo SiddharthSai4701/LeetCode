d2a74ad57f3adcc263118ab4d6df95a2eeb63037
Siddharth Vijay Sai <sidvsai@gmail.com>
2025-01-14 00:21:05 +0530

    LeetCode 338 Ransom Note

--- Python/Easy/P383_RansomNote.py
+++ Python/Easy/P383_RansomNote.py
@@ -25,7 +25,42 @@ Constraints:
 ransomNote and magazine consist of lowercase English letters.
 """
 
+# My Code
+# class Solution(object):
+#     def canConstruct(self, ransomNote, magazine):
+#         """
+#         :type ransomNote: str
+#         :type magazine: str
+#         :rtype: bool
+#         """
 
+#         d = dict()
+
+#         for i in magazine:
+#             if i not in d.keys():
+#                 d[i] = 1
+#             else:
+#                 d[i] += 1
+
+#         for i in ransomNote:
+#             if i not in d.keys():
+#                 return False
+
+#             elif d[i] > 1:
+#                 d[i]-=1
+
+#             elif d[i] == 1:
+#                 del d[i]
+#         return True
+
+
+# s = Solution()
+# print(s.canConstruct(ransomNote="aa", magazine="ab"))
+
+
+# Alternate  ways to build up the dictionary
+
+from collections import defaultdict, Counter
 class Solution(object):
     def canConstruct(self, ransomNote, magazine):
         """
@@ -33,7 +68,28 @@ class Solution(object):
         :type magazine: str
         :rtype: bool
         """
-        
+
+        # In an ordinary dict, accessing a key that doesn't exist will throw an error
+        # But that doesn't happen in a defaultdict
+        d = defaultdict(int)
+        for i in magazine:
+            d[i]+=1
+
+        # An even simpler method is to use Counter
+        # d = Counter(magazine)
+
+
+        for i in ransomNote:
+            if i not in d.keys():
+                return False
+
+            elif d[i] > 1:
+                d[i] -= 1
+
+            elif d[i] == 1:
+                del d[i]
+        return True
+
 
 s = Solution()
-s.canConstruct(ransomNote="a", magazine="b")
+print(s.canConstruct(ransomNote="aa", magazine="ab"))
