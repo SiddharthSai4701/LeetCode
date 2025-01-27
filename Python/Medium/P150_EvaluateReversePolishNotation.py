bec7ebd9f266f2ef028c55ef6c13607954afe1ef
Siddharth Vijay Sai <sidvsai@gmail.com>
2025-01-27 21:04:36 +0530

    Corrected silly error

--- Python/Medium/P150_EvaluateReversePolishNotation.py
+++ Python/Medium/P150_EvaluateReversePolishNotation.py
@@ -91,7 +91,7 @@ class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stk = []
         for t in tokens:
-            if t in "+=*/":
+            if t in "+-*/":
                 b, a = stk.pop(), stk.pop()
 
                 if t == "+":
