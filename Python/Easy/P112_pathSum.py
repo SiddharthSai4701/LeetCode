3bfc037ee883c1081130de3454a2123e09a70b36
Siddharth Vijay Sai <sidvsai@gmail.com>
2025-06-15 18:00:49 +0530

    Solved 112

---
+++ Python/Easy/P112_pathSum.py
@@ -0,0 +1,94 @@
+"""
+Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+A leaf is a node with no children.
+
+
+
+Example 1:
+
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+Output: true
+Explanation: The root-to-leaf path with the target sum is shown.
+Example 2:
+
+
+Input: root = [1,2,3], targetSum = 5
+Output: false
+Explanation: There are two root-to-leaf paths in the tree:
+(1 --> 2): The sum is 3.
+(1 --> 3): The sum is 4.
+There is no root-to-leaf path with sum = 5.
+Example 3:
+
+Input: root = [], targetSum = 0
+Output: false
+Explanation: Since the tree is empty, there are no root-to-leaf paths.
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5000].
+-1000 <= Node.val <= 1000
+-1000 <= targetSum <= 1000
+"""
+
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+# My First Solution - passed all tests
+# Approach similar to P 100  except that instead of comparing the left nodes of two different trees, we compare the
+# left and right nodes of the same tree\
+
+# class Solution:
+#     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+
+
+#         def calcSum(root):
+
+#             if not root:
+#                 return 0
+            
+#             s = root.value + calcSum(root.left) + calcSum(root.right)
+
+#             return s
+        
+#         s = calcSum(root)
+
+#         if s == targetSum:
+#             return True
+        
+#         return False
+            
+
+
+# Greg's Solution
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        
+        def hasSum(root, cur_sum):
+
+            if not root:
+                return False
+
+            cur_sum += root.val
+
+            if not root.left and not root.right:
+                return cur_sum == targetSum
+            
+            return hasSum(root.left, cur_sum) or hasSum(root.right, cur_sum)
+        
+        return hasSum(root, 0)
+
+
+# Test Case 1
+
