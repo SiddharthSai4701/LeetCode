fce81e7c3d440a0f820d47dac504b95e22437946
Siddharth Vijay Sai <sidvsai@gmail.com>
2025-06-18 22:38:16 +0530

    Solved 145

--- Python/Easy/P145_BinaryTreePostorderTraversal.py
+++ Python/Easy/P145_BinaryTreePostorderTraversal.py
@@ -1,5 +1,5 @@
 """
-Given the root of a binary tree, return the preorder traversal of its nodes' values.
+Given the root of a binary tree, return the postorder traversal of its nodes' values.
 
 
 
@@ -7,7 +7,7 @@ Example 1:
 
 Input: root = [1,null,2,3]
 
-Output: [1,2,3]
+Output: [3,2,1]
 
 Explanation:
 
@@ -17,7 +17,7 @@ Example 2:
 
 Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
 
-Output: [1,2,4,5,6,7,3,8,9]
+Output: [4,6,7,5,2,9,8,3,1]
 
 Explanation:
 
@@ -39,12 +39,13 @@ Output: [1]
 
 Constraints:
 
-The number of nodes in the tree is in the range [0, 100].
+The number of the nodes in the tree is in the range [0, 100].
 -100 <= Node.val <= 100
 
 
 Follow up: Recursive solution is trivial, could you do it iteratively?
 """
+
 from typing import Optional, List
 
 
@@ -56,20 +57,19 @@ class TreeNode:
 
 
 class Solution:
-    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
 
         nodes = []
 
-        def pre_order_traversal(root):
-
+        def post_order_traversal(root):
             if not root:
                 return
 
+            post_order_traversal(root.left)
+            post_order_traversal(root.right)
             nodes.append(root.val)
-            pre_order_traversal(root.left)
-            pre_order_traversal(root.right)
 
-        pre_order_traversal(root)
+        post_order_traversal(root)
 
         return nodes
 
@@ -88,4 +88,4 @@ B.left = D
 B.right = E
 
 s = Solution()
-print(s.preorderTraversal(A))
+print(s.postorderTraversal(A))
