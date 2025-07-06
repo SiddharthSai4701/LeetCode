26c4c7a6716460496cf001a4135e1a921cdf604a
Siddharth Vijay Sai <sidvsai@gmail.com>
2025-07-07 01:09:59 +0530

    Solved 973

--- Python/Medium/P973_KClosestPointsToOrigin.py
+++ Python/Medium/P973_KClosestPointsToOrigin.py
@@ -31,10 +31,38 @@ Constraints:
 """
 from typing import List
 import heapq
+import math
+
+# My Solution - Time: O(N log N)
+# class Solution:
+#     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+#         heap = []
+
+#         for point in points:
+#             dist = math.sqrt(pow((0 - point[0]), 2) + pow((0 - point[1]), 2))
+#             heapq.heappush(heap, (dist, point))
+
+#         return [heapq.heappop(heap)[1] for _ in range(k)]
+
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        def dist(x, y):
+            return x**2 + y**2
+        
+        heap = []
+        for x, y in points:
+            d = dist(x, y)
+            if len(heap) < k:
+                heapq.heappush(heap, (-d, x, y))
+            else:
+                heapq.heappushpop(heap, (-d, x, y))
+        return [(x, y) for d, x, y in heap]
+
 
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         pass
 
 s = Solution()
-print(s.kClosest())
+print(s.kClosest(points=[[3, 3], [5, -1], [-2, 4]], k=2))
