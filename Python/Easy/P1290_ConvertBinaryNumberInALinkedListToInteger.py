fddec4a3b9f72d06be31a604c4c66542c7937a3e
Siddharth Vijay Sai <sidvsai@gmail.com>
2025-07-14 20:48:12 +0530

    Solved 1290

--- Python/Easy/P1290_ConvertBinaryNumberInALinkedListToInteger.py
+++ Python/Easy/P1290_ConvertBinaryNumberInALinkedListToInteger.py
@@ -25,3 +25,48 @@ The Linked List is not empty.
 Number of nodes will not exceed 30.
 Each node's value is either 0 or 1.
 """
+
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def getDecimalValue(self, head: Optional[ListNode]) -> int:
+        num = ""
+        while head:
+            num += str(head.val)
+            head = head.next
+
+        decimalEquivalent = 0
+        for i in range(0, len(num)):
+            print(i)
+            if num[i] == "1":
+                decimalEquivalent += pow(2, len(num) - i - 1)
+        return decimalEquivalent
+
+
+# n1 = ListNode(1)
+# n2 = ListNode(0)
+# n3 = ListNode(1)
+
+# n1.next = n2
+# n2.next = n3
+
+arr = [1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0]
+# arr = [1, 0, 1]
+
+# Create all nodes
+nodes = [ListNode(val) for val in arr]
+
+# Link them together
+for i in range(len(nodes) - 1):
+    nodes[i].next = nodes[i + 1]
+
+s = Solution()
+print(s.getDecimalValue(nodes[0]))
